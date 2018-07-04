c40f7143f6267f41dc61ce1529aa04e0e9e36a97
conao <conao3@gmail.com>
2018-07-04 23:12:57 +0900

    add unique key for cookie

--- orgile-includes/orgile-config.php
+++ orgile-includes/orgile-config.php
@@ -3,4 +3,13 @@
 ChromePhp::info('load orgile-config.php');
 
 define('ORGILE_CONTENT', ABSPATH . 'orgile-content/contents/');
-ChromePhp::info(ORGILE_CONTENT);
+
+// Unique keys for Cookie
+define('AUTH_KEY',         '~uy;W](Z|HmeM<,`mTvaBKsbQ,h%}_Cq5|o!m%GzkclT,kbh^hcBnVpW>W`#!QXk');
+define('SECURE_AUTH_KEY',  'DH`d;AV*|x[oshf>No#*[=rzQ?F#~MY=$E -//HMWL!`O^D&/rq&BX]x=n^ft6Y(');
+define('LOGGED_IN_KEY',    'n>Z/YTx-IFH>:&*-zdk)ukD+Li6@?AGt;_u.OY9*RFA(LNhG8QfSK5!rlupGt+ =');
+define('NONCE_KEY',        'Uq/%PxMF9z/O[0I51_~xv]Pr~03=^{c0{sG?byl#p3b|-?umY0|pb2IY/Rb_ro1T');
+define('AUTH_SALT',        '2_FD*m~SLB9JO47q%98!L%CR*d;*EVH.uvRdW8_U,^~][iL-;$QeYbX:v4Wb),sW');
+define('SECURE_AUTH_SALT', 'Q<33!U[XQnV_R~eN/jBRmoP~E?m4t*+)yiCaAgyz<i+*)T`G73tvVrurQYS Gykd');
+define('LOGGED_IN_SALT',   'h1A7Kt&P|UEz6KGK^IIgWIDIaL<y}W7<MEX?09Df.8bWIP%pq!hrum>H@+6PD?,Y');
+define('NONCE_SALT',       'XItr6cqg-qm+aRi mE}a=|<2U?K67m~L@xUpindHvlEH1/*,vt$@v`#NQMyI1RjP');
