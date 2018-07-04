bfc1a47503fe41f922e5488feba34fa1a73ac01c
conao <conao3@gmail.com>
2018-07-04 23:52:51 +0900

    add chromephp comment

--- orgile-includes/orgile-main.php
+++ orgile-includes/orgile-main.php
@@ -3,6 +3,11 @@
 // ChromePhp::info('test');
 // ChromePhp::warn('test');
 // ChromePhp::error('test');
+// ChromePhp::groupCollapsed('MyGroup');
+// for ($i = 1; $i <= 10; $i++) {
+//     ChromePhp::log('log' . $i);
+// }
+// ChromePhp::groupEnd();
 
 define('ORGILE_INC', dirname(__FILE__) . '/');
 
