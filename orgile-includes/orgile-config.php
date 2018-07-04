d98cf60a7b6cd848f926c1cc38ff75cee4482057
conao <conao3@gmail.com>
2018-07-04 23:41:57 +0900

    define themedir

--- orgile-includes/orgile-config.php
+++ orgile-includes/orgile-config.php
@@ -4,6 +4,7 @@ ChromePhp::info('load orgile-config.php');
 
 // define constants
 define('CONTENTDIR', ABSPATH . 'orgile-content/contents/');
+define('THEMEDIR', ABSPATH . 'orgile-content/themes/');
 
 // Unique keys for Cookie
 define('AUTH_KEY',         '~uy;W](Z|HmeM<,`mTvaBKsbQ,h%}_Cq5|o!m%GzkclT,kbh^hcBnVpW>W`#!QXk');
