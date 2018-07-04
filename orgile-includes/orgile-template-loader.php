664909becfdcf472b590a71edddadeeaf71ffd53
conao <conao3@gmail.com>
2018-07-05 00:27:58 +0900

    implement template-loader

--- orgile-includes/orgile-template-loader.php
+++ orgile-includes/orgile-template-loader.php
@@ -1,3 +1,17 @@
 <?php
 
 ChromePhp::info('load orgile-template-loader.php');
+
+if     (is_404()      && $template = get_404_template()):
+elseif (is_search()   && $template = get_search_template()):
+elseif (is_home()     && $template = get_home_template()):
+elseif (is_single()   && $template = get_single_template()):
+elseif (is_page()     && $template = get_page_template()):
+elseif (is_singular() && $template = get_singular_template()):
+elseif (is_category() && $template = get_category_template()):
+elseif (is_tag()      && $template = get_tag_template()):
+elseif (is_author()   && $template = get_author_template()):
+elseif (is_date()     && $template = get_date_template()):
+elseif (is_archive()  && $template = get_archive_template()):
+else:  $template = get_index_template();
+endif;
