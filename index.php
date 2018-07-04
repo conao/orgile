d9111a946da4ab4ddc9fa5c4a3602ae700746d51
conao <conao3@gmail.com>
2018-07-04 21:53:45 +0900

    create orgile-main.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php // Time-stamp: <2011-11-27 Sun 15:55 index.php>
-include('site/orgile/orgile.php');
-pageHandler();
-?>
+<?php
+// include('site/orgile/orgile.php');
+// pageHandler();
+include('orgile-includes/orgile-main.php');
