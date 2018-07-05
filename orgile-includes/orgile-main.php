b314c8d83fec076a1f576e62f92194a93df5b8d4
conao <conao3@gmail.com>
2018-07-05 19:00:13 +0900

    uninclude orgile-query.php

--- orgile-includes/orgile-main.php
+++ orgile-includes/orgile-main.php
@@ -21,7 +21,7 @@ include(ORGILE_INC . 'class-orgile-query.php');
 
 // include orgile sources
 include(ORGILE_INC . 'orgile-config.php');
-include(ORGILE_INC . 'orgile-query.php');
+// include(ORGILE_INC . 'orgile-query.php');
 
 // load template
 include(ORGILE_INC . 'orgile-template-loader.php');
