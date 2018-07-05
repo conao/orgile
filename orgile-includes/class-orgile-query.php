577a241b25a23336c2f852226e9a54ad9d02dd30
conao <conao3@gmail.com>
2018-07-05 11:10:54 +0900

    implement class-orgile-query

--- orgile-includes/class-orgile-query.php
+++ orgile-includes/class-orgile-query.php
@@ -1,3 +1,22 @@
 <?php
 
 ChromePhp::info('load class-orgile-query');
+
+class ORGILE_Query {
+    public $is_404      = false;
+    public $is_search   = false;
+    public $is_home     = false;
+    public $is_single   = false;
+    public $is_page     = false;
+    public $is_singular = false;
+    public $is_category = false;
+    public $is_tag      = false;
+    public $is_author   = false;
+    public $is_date     = false;
+    public $is_archive  = false;
+    
+    function __construct() {
+        ChromePhp::info('create ORGILE_query');
+        ChromePhp::info($_SERVER['REQUEST_URI']);
+    }
+}
--- orgile-includes/orgile-template-loader.php
+++ orgile-includes/orgile-template-loader.php
@@ -2,16 +2,19 @@
 
 ChromePhp::info('load orgile-template-loader.php');
 
-if     (is_404()      && $template = get_404_template()):
-elseif (is_search()   && $template = get_search_template()):
-elseif (is_home()     && $template = get_home_template()):
-elseif (is_single()   && $template = get_single_template()):
-elseif (is_page()     && $template = get_page_template()):
-elseif (is_singular() && $template = get_singular_template()):
-elseif (is_category() && $template = get_category_template()):
-elseif (is_tag()      && $template = get_tag_template()):
-elseif (is_author()   && $template = get_author_template()):
-elseif (is_date()     && $template = get_date_template()):
-elseif (is_archive()  && $template = get_archive_template()):
-else:  $template = get_index_template();
+global $orgile_query;
+$orgile_query = new ORGILE_Query();
+
+if     ($orgile_query->is_404      && load_404_template()):
+elseif ($orgile_query->is_search   && load_search_template()):
+elseif ($orgile_query->is_home     && load_home_template()):
+elseif ($orgile_query->is_single   && load_single_template()):
+elseif ($orgile_query->is_page     && load_page_template()):
+elseif ($orgile_query->is_singular && load_singular_template()):
+elseif ($orgile_query->is_category && load_category_template()):
+elseif ($orgile_query->is_tag      && load_tag_template()):
+elseif ($orgile_query->is_author   && load_author_template()):
+elseif ($orgile_query->is_date     && load_date_template()):
+elseif ($orgile_query->is_archive  && load_archive_template()):
+else:  load_index_template();
 endif;
