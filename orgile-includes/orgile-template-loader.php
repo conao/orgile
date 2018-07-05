9f627bef5bf666cb5c517b823c8f64d5e4a614f1
conao <conao3@gmail.com>
2018-07-05 14:46:20 +0900

    implement $template variable

--- orgile-includes/orgile-template-loader.php
+++ orgile-includes/orgile-template-loader.php
@@ -5,16 +5,18 @@ ChromePhp::info('load orgile-template-loader.php');
 global $orgile_query;
 $orgile_query = new ORGILE_Query();
 
-if     ($orgile_query->is_404      && load_template('404')):
-elseif ($orgile_query->is_search   && load_template('search')):
-elseif ($orgile_query->is_home     && load_template('home')):
-elseif ($orgile_query->is_single   && load_template('single')):
-elseif ($orgile_query->is_page     && load_template('page')):
-elseif ($orgile_query->is_singular && load_template('singular')):
-elseif ($orgile_query->is_category && load_template('category')):
-elseif ($orgile_query->is_tag      && load_template('tag')):
-elseif ($orgile_query->is_author   && load_template('author')):
-elseif ($orgile_query->is_date     && load_template('date')):
-elseif ($orgile_query->is_archive  && load_template('archive')):
-else:  load_template('index');
+if     ($orgile_query->is_404):      $template = ['404',      'index'];
+elseif ($orgile_query->is_search):   $template = ['search',   'index'];
+elseif ($orgile_query->is_home):     $template = ['home',     'index'];
+elseif ($orgile_query->is_single):   $template = ['single',   'singular', 'index'];
+elseif ($orgile_query->is_page):     $template = ['page',     'singular', 'index'];
+elseif ($orgile_query->is_singular): $template = ['singular', 'index'];
+elseif ($orgile_query->is_category): $template = ['category', 'archive', 'index'];
+elseif ($orgile_query->is_tag):      $template = ['tag',      'archive', 'index'];
+elseif ($orgile_query->is_author):   $template = ['author',   'archive', 'index'];
+elseif ($orgile_query->is_date):     $template = ['date',     'archive', 'index'];
+elseif ($orgile_query->is_archive):  $template = ['archive',  'index'];
+else:   $template = ['index'];
 endif;
+
+ChromePhp::info($template);
