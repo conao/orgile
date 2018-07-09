f99714745ca5ba5a58646579122fb4c0b26c0d8f
conao <conao3@gmail.com>
2018-07-09 19:00:08 +0900

    implement page_*_handler

--- orgile-includes/class-orgile-query.php
+++ orgile-includes/class-orgile-query.php
@@ -25,5 +25,41 @@ class ORGILE_Query {
         
         ChromePhp::warn($request);
         ChromePhp::warn($req_path, $req_params);
+
+        $this->parse_request($req_path, $req_params);
+    }
+
+    function parse_request($path, $params) {
+        $type = ['archive', 'search', '404', ''];
+        
+        foreach ($type as $target) {
+            if ($path[0] == $target) {
+                call_user_func_array([$this, "parse_" . $target . "_handler"], [$path, $params]);
+                return;
+            }
+        }
+
+        // not match
+        call_user_func_array([$this, "parse_undefined_handler"], [$path, $params]);
+    }
+    
+    function parse_archive_handler($path, $params) {
+        ChromePhp::info("handler: " . __FUNCTION__);
+    }
+
+    function parse_search_handler($path, $params) {
+        ChromePhp::info("handler: " . __FUNCTION__);
+    }
+
+    function parse_404_handler($path, $params) {
+        ChromePhp::info("handler: " . __FUNCTION__);
+    }
+
+    function parse__handler($path, $params) {
+        ChromePhp::info("handler: " . __FUNCTION__);
+    }
+
+    function parse_undefined_handler($path, $params) {
+        ChromePhp::info("handler: " . __FUNCTION__);
     }
 }
