5e68a1e3f95f131d914002110d2cea2d056dc115
conao <conao3@gmail.com>
2018-07-10 11:10:36 +0900

    delete unused variables

--- orgile-includes/class-orgile-query.php
+++ orgile-includes/class-orgile-query.php
@@ -5,10 +5,7 @@ ChromePhp::info('load class-orgile-query');
 class ORGILE_Query {
     public $is_404      = false;
     public $is_search   = false;
-    public $is_home     = false;
-    public $is_single   = false;
     public $is_page     = false;
-    public $is_singular = false;
     public $is_category = false;
     public $is_tag      = false;
     public $is_author   = false;
