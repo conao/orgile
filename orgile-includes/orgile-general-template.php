c91b2ece0879e5815407c97eae224438d90148ed
conao <conao3@gmail.com>
2018-07-11 16:11:29 +0900

    implement locate_template, load_template

--- orgile-includes/orgile-general-template.php
+++ orgile-includes/orgile-general-template.php
@@ -1,3 +1,31 @@
 <?php
 
 ChromePhp::info('load orgile-general-template.php');
+
+function locate_template($template_names, $load = false, $require_once = true) {
+    // Retrieve the name of the highest priority template file that exists.
+    
+    $located = '';
+    foreach ((array)$template_names as $template_name) {
+        if (file_exists(THEMEDIR . $template_name)) {
+            $located = $template_name;
+            break;
+        }
+    }
+
+    if ($load && $located !== '') {
+        load_template($located, $require_once);
+    }
+
+    return $located;
+}
+
+function load_template($template_name, $require_once = true) {
+    // Require the template file.
+
+    if ($require_once) {
+        require_once($template_name);
+    } else {
+        require($template_name);
+    }
+}
